b5488c29463fa969d6712b7190fab9643f81ed63
Artem Romanenia <artem.romanenia@gmail.com>
2018-07-29 13:52:49 +0300

    Small solution changes

--- MediatR.Extensions.Microsoft.AspNetCore.Mvc/MvcOptionsExtensions.cs
+++ MediatR.Extensions.Microsoft.AspNetCore.Mvc/MvcOptionsExtensions.cs
@@ -12,14 +12,16 @@ namespace Mediatr.Extensions.Microsoft.AspNetCore.Mvc
         /// Adds default MediatR Mvc convention.
         /// </summary>
         /// <param name="options">options</param>
-        public static void AddMediatrMvcConvention(this MvcOptions options) => AddMediatrMvcConvention(options, null, null);
+        public static void AddMediatrMvcConvention(this MvcOptions options)
+            => AddMediatrMvcConvention(options, null, null);
 
         /// <summary>
         /// Adds default MediatR Mvc convention.
         /// </summary>
         /// <param name="options">options</param>
         /// <param name="provideControllerName">Provides desired controller name based on <see cref="IRequest{TResponse}"/> type.</param>
-        public static void AddMediatrMvcConvention(this MvcOptions options, Func<Type, string> provideControllerName) => AddMediatrMvcConvention(options, provideControllerName, null);
+        public static void AddMediatrMvcConvention(this MvcOptions options, Func<Type, string> provideControllerName)
+            => AddMediatrMvcConvention(options, provideControllerName, null);
 
         /// <summary>
         /// Adds default MediatR Mvc convention.
@@ -27,7 +29,8 @@ namespace Mediatr.Extensions.Microsoft.AspNetCore.Mvc
         /// <param name="options">options</param>
         /// <param name="classifyRequestType">Classifies <see cref="IRequest{TResponse}"/> type,
         /// so that it can be constrained to appropriate Http Verb.</param>
-        public static void AddMediatrMvcConvention(this MvcOptions options, Func<Type, RequestType> classifyRequestType) => AddMediatrMvcConvention(options, null, classifyRequestType);
+        public static void AddMediatrMvcConvention(this MvcOptions options, Func<Type, RequestType> classifyRequestType)
+            => AddMediatrMvcConvention(options, null, classifyRequestType);
 
         /// <summary>
         /// Adds default MediatR Mvc convention.
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MediatR.Extensions.Microsoft.AspNetCore.Mvc
 MediatR.Extensions.Microsoft.AspNetCore.Mvc is the extension that marries MediatR to ASP.NET Core in a way that keeps MediatR totally agnostic of how it is being used.
 
-Easily configurable, it wraps each MediatR request/handler in a constructed generic controller and provides several flexibility points.
+Easily configurable, it wraps each MediatR request in a constructed generic controller and provides several flexibility points.
 
 See [Wiki](https://github.com/Artem-Romanenia/MediatR.Extensions.Microsoft.AspNetCore.Mvc/wiki) to get started.
